57f974b53408960584bd5e9449eb9199c26f3861
ravendraksingh <ravendraksingh@gmail.com>
2022-12-09 20:25:18 +0530

    added bootstrap.properties file

--- payment-service/src/main/java/com/rks/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/rks/paymentservice/PaymentServiceApplication.java
@@ -2,6 +2,7 @@ package com.rks.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.hateoas.config.EnableHypermediaSupport;
 

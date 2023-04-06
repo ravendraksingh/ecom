b709f113ad90ea378bd1ffe7a9746736a6a51877
ravendraksingh <ravendraksingh@gmail.com>
2023-04-06 11:47:34 +0530

    removed infra setup from services repo

--- apigateway/src/main/java/io/rks/apigateway/constants/ApiGatewayErrorCodes.java
+++
@@ -1,5 +0,0 @@
-package io.rks.apigateway.constants;
-
-public class ApiGatewayErrorCodes {
-    public static final String AUTHORIZATION_HEADER_MISSING = "GNAUE0001";
-}
--- gateway/src/main/java/com/rks/gateway/config/EurekaClientConfig.java
+++
@@ -1,1 +0,0 @@
-package com.rks.gateway.config;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;@EnableDiscoveryClientpublic class EurekaClientConfig {}
